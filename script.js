a26d435787017ea4b99e3590d3f67f2af4e5766a
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-07-22 08:13:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//define variables
+let bill = document.querySelector('#bill');
+let custom = document.querySelector('#custom');
+let people = document.querySelector('#people');
+//take input
+
+//calculate input
+
+//give output
+
+//reset
