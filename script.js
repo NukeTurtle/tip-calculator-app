0e304e58f0f704d3b993c54c5da09d6d14a228b7
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-07-22 10:07:25 +0200

    Add files via upload
    - mobile version needs some detail fixing
    - javascript not working properly yet

--- script.js
+++ script.js
@@ -11,6 +11,30 @@ total.innerHTML = (bill * .05 / people) + (bill / people);
 }
 claculate5();
 
+function claculate10(){
+    tip.innerHTML = "$ " + bill * .1 / people;
+    total.innerHTML = (bill * .1 / people) + (bill / people);
+    }
+    claculate10();
+
+    function claculate15(){
+        tip.innerHTML = "$ " + bill * .15 / people;
+        total.innerHTML = (bill * .15 / people) + (bill / people);
+        }
+        claculate15();
+
+        function claculate20(){
+            tip.innerHTML = "$ " + bill * .2 / people;
+            total.innerHTML = (bill * .2 / people) + (bill / people);
+            }
+            claculate5();
+
+            function claculate25(){
+                tip.innerHTML = "$ " + bill * .25 / people;
+                total.innerHTML = (bill * .25 / people) + (bill / people);
+                }
+                claculate25();
+
 function clearAll(){
     tip.innerHTML = "$ 0";
     total.innerHTML = "$ 0";
