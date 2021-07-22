2a0be561f725a66a0f97d6ef2af1df502287cc5e
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-07-22 09:46:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-//define variables
-let bill = document.querySelector('#bill');
-let custom = document.querySelector('#custom');
-let people = document.querySelector('#people');
-//take input
+// //define variables
+var bill = document.querySelector('#bill').value;
+var custom = document.querySelector('#custom');
+var people = document.querySelector('#people').value;
+var tip = document.querySelector('#tip');
+var total = document.querySelector('#total');
 
-//calculate input
+function claculate5(){
+tip.innerHTML = "$ " + bill * .05 / people;
+total.innerHTML = (bill * .05 / people) + (bill / people);
+}
+claculate5();
 
-//give output
-
-//reset
+function clearAll(){
+    tip.innerHTML = "$ 0";
+    total.innerHTML = "$ 0";
+    bill = "0";
+    people = "0";
+}
